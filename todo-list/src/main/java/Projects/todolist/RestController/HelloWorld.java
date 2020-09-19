80129e791f970ceeb90dc6895ef18a2a68a51c55
Vishnu T <vishnuthamiz96@gmail.com>
2020-09-19 22:38:23 +0530

    todo list

---
+++ todo-list/src/main/java/Projects/todolist/RestController/HelloWorld.java
@@ -0,0 +1,18 @@
+package Projects.todolist.RestController;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HelloWorld {
+
+    @GetMapping("/hello")
+    public String hello() {
+
+        return "Hello world spring boot";
+    }
+
+}
